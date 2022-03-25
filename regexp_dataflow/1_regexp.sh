fd8e13baeb2acc0e9a22a1c7a1a076b5ab5ba3cb
Theo Yu <ty364@cornell.edu>
2022-03-25 11:35:34 +0800

    修改正则

--- regexp_dataflow/1_regexp.sh
+++ regexp_dataflow/1_regexp.sh
@@ -13,21 +13,20 @@ set hive.exec.dynamic.partition.mode=nonstrict;
 insert overwrite table ${new_database_name}.${new_table_name} partition(the_date,file_no) select * from ${source_database_name}.${source_table_name} where 
 abnormal_label regexp '正常文本' AND
 (
-msg regexp '车' OR
-app_name regexp '车' OR
-suspected_app_name regexp '车'
+app_name regexp '汽车' OR
+app_name regexp '?<[约专快打租单托拖运拼风叫机停泊有用行客货派公中吊卡重叉通出]车$' OR
+suspected_app_name regexp '汽车' OR
+suspected_app_name regexp '?<[约专快打租单托拖运拼风叫机停泊有用行客货派公中吊卡重叉通出]车$' OR
+msg regexp '汽车' OR
+msg regexp '看车|试驾|试乘|提车|交易车辆|买车|购车|支付.*车款|售车|车抢购|购得爱车|喜得爱车|金融方案' OR 
+msg regexp '约看(?![牙诊])|带看|询价|(询问|咨询).*价格|咨询.*销售顾问|欲购买|摇号|指标|竞价|竞得|竞拍|上拍|流拍|开拍|拍卖|流拍|报价|出价|成交|撮合邀请|感谢您来到'
 ) AND
 (
-msg not regexp '([约专快打租单托拖运拼风叫机停泊有用行客货派公中吊卡重叉])车' AND
-app_name not regexp '([约专快打租单托拖运拼风叫机停泊有用行客货派公中吊卡重叉])车' AND
-suspected_app_name not regexp '([约专快打租单托拖运拼风叫机停泊有用行客货派公中吊卡重叉])车'
-) AND
-(
-msg not regexp '污染天气.{1,4}预警|车费未支付|投保|保险|续保|保费|车险|流量直通车|预警|公交|基金|已退款成功|验证码|点击领取|(?<=参加“).*?(?=”活动)|安全|侧滑甩尾|回[a-zA-Z]退|[a-zA-Z]{2}退订|回复[a-zA-Z]{2}退订|取消预约|文明|车主提醒|您[的有].{2,6}包裹|快递|核酸|ETC|贷款|车贷|罚款|扣款交易|违法行为|挪车|如实申报|优享型司机|接单条件|卸\d{1,10}吨|增\d{1,10}吨|抢单|口罩|等待救援|取件码|车门未上锁|检票口|物流|货运|退订|金融|交通违法|社区居民委员会|行为规范|尽快还车|计费|智能盒|液化气|石化|工业|装车费|视频|提单号|防疫物资|支付乘车费用|装卸车|升降车|旅行车|公务车|助力车|自行车|共享.{1,2}车|出车' AND
-app_name not regexp '手机报|快报|保险|流量直通车|公交|基金|金融|ETC|资讯|车管所|铁路|视频' AND
-suspected_app_name not regexp '手机报|快报|保险|流量直通车|公交|基金|金融|ETC|资讯|车管所|铁路|视频'
+msg not regexp '乘*车[贷险票费]|房' AND
+msg not regexp '验证码|登陆凭证|动态密码|激活码|取件码' AND
+msg not regexp '退订|回复*[a-zA-Z]{1,4}退|拒收回[a-zA-Z]{1,4}|点击(立即)*领取|点击链接'
 )
-;
+; 
 "
 
 cd /home/${rss_path}/ && bash rss.sh "data_explorer" "nlp_dev" "$sql_part"
